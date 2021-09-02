02eb8cc5b95e8be21dbc3fb88415a5d9d4e49493
Jochizan <remnyachizot2015@gmail.com>
2021-09-01 20:20:02 -0500

    SSR vs SSG with Next.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import fetch from 'isomorphic-unfetch'
 import Layout from '@components/Layout/Layout';
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader';
 import ProductList from '@components/ProductList/ProductList';
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([]);
+//export const getServerSideProps = async () => {
+  //const res = await fetch('https://platzi-avo.vercel.app/api/avo');
+  //const { data: productList }: TAPIAvoResponse = await res.json();
 
-  useEffect(() => {
-    fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data);
-      });
-  }, []);
+  //return {
+    //props: {
+      //productList
+    //}
+  //};
+//};
 
+// Página estatica
+export const getStaticProps = async () => {
+  const res = await fetch('https://platzi-avo.vercel.app/api/avo');
+  const { data: productList }: TAPIAvoResponse = await res.json();
+
+  return {
+    props: {
+      productList
+    }
+  };
+};
+
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
     <Layout>
       <KawaiiHeader />
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,28 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
+import React from 'react';
+import { GetStaticProps } from 'next';
 
-import Layout from '@components/Layout/Layout'
-import ProductSummary from '@components/ProductSummary/ProductSummary'
+import Layout from '@components/Layout/Layout';
+import ProductSummary from '@components/ProductSummary/ProductSummary';
 
-const ProductPage = () => {
-  const { query } = useRouter()
-  const [product, setProduct] = useState<TProduct | null>(null)
+// Página dinamica with getStaticPaths
 
-  useEffect(() => {
-    if (query.id) {
-        fetch(`/api/avo/${query.id}`)
-        .then((response) => response.json())
-        .then((data: TProduct) => {
-          setProduct(data)
-        })
+export const getStaticPaths = async () => {
+  const res = await fetch('https://platzi-avo.vercel.app/api/avo');
+  const { data: productList }: TAPIAvoResponse = await res.json();
+
+  const paths = productList.map(({ id }) => ({ params: {
+    id
+  }}))
+
+  return {
+    paths,
+    // incremental static generation
+    // cualquier página que no se especifique botará 404
+    fallback: false
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params?.id as string;
+  const res = await fetch(`https://platzi-avo.vercel.app/api/avo/${id}`);
+  const product: TProduct = await res.json();
+
+  return {
+    props: {
+      product
     }
-  }, [query.id])
+  };
+};
 
+const ProductPage = ({ product }: { product: TProduct }) => {
   return (
     <Layout>
       {product == null ? null : <ProductSummary product={product} />}
     </Layout>
-  )
-}
+  );
+};
 
-export default ProductPage
+export default ProductPage;
